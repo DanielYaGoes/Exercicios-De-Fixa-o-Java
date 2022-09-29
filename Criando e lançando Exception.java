936899a0d76fc694838ac01573a7e0ed7a392452
DanielYaGoes <110986186+DanielYaGoes@users.noreply.github.com>
2022-09-29 11:35:50 -0300

    Update Criando e lançando Exception.java

--- Criando e lançando Exception.java
+++ Criando e lançando Exception.java
@@ -1,11 +1,13 @@
+//O objetivo é simular um banco no qual o usuario possuirá um saldo e ao sacar um valor , se o valor for maior que o saldo lançará um execeção
+
 import java.util.Scanner;
 
 public class CriandoELancandoExcecao {
 
 	// Criando um ambiente que representa um banco no qual possui conta e saldo
-	private double saldo;
+	private double saldo;//por padrão do java o double incia como 0
 	private String nome;
-
+	//Criando metodo saca 
 	public void saca(double valor) throws SaldoInsuficienteException {
 		if (this.saldo < valor) {
 			throw new SaldoInsuficienteException("Saldo insuficiente");
@@ -14,19 +16,22 @@ public class CriandoELancandoExcecao {
 			System.out.println("A transasao foi um sucesso seu saldo atual eh de: "+ this.getSaldo());
 		}
 	}
-
+	//metodo para depositar um valor no saldo
 	public void deposita(double valor) {
 		this.saldo += valor;
 	}
-
+	//metodo para ver o saldo
 	public double getSaldo() {
 		return this.saldo;
 	}
-
+	//Testando o código
 	public static void main(String[] args) throws CriandoELancandoExcecao.SaldoInsuficienteException {
 		CriandoELancandoExcecao conta1 = new CriandoELancandoExcecao();
 		Scanner leitor = new Scanner(System.in);
-		conta1.deposita(12000);
+		System.out.println("Sua Conta tem " + conta1.getSaldo() + " de saldo");
+		System.out.println("Quanto voce deseja depositar?");
+		double deposito = leitor.nextDouble();
+		conta1.deposita(deposito);
 		System.out.println("Sua Conta tem " + conta1.getSaldo() + " de saldo");
 		System.out.println("Escreva quanto deseja sacar");
 		double valor= leitor.nextDouble();
@@ -34,7 +39,7 @@ public class CriandoELancandoExcecao {
 		
 		
 	}
-
+	// Criando a exceção para ser lançada
 	class SaldoInsuficienteException extends Exception {
 
 		public SaldoInsuficienteException(String msg) {
